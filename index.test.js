7e2992e103b38dfbf38edf6d7a0a939d45b86597
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-13 16:55:24 -0500

    Unitests cases for ScoutController

--- index.test.js
+++ index.test.js
@@ -5,35 +5,42 @@ const request = require("supertest")("http://localhost:9000/api");
 const Server = require('./Model/Server');
 const server = new Server();
 const app = server.app;
-dbConnection();
 
 describe("ScoutController unit tests", () => {
     let scout_ ={
-        "nombre": "Prueba1",
-        "apellido":"Test1",
-        "email": "scout@correo.com",
-        "password":"12345678Test",
-        "link_ficha_medica":"www.link.com",
-        "fecha_nacimiento": "08-06-1988",
-        "celular": "323554989",
-        "ramaAsociada":"Rama"
+        nombre: "Prueba1",
+        apellido:"Test1",
+        email: "scout@correo.com",
+        password:"12345678Test",
+        link_ficha_medica:"www.link.com",
+        fecha_nacimiento: "08-06-1988",
+        celular: "323554989",
+        ramaAsociada:"Rama"
     };
     describe("CreateScout", () => {
-        it("should create a new scout", (done) => {
-            request(app)
-            .post("/scouts/create-scout").send(scout_).expect(201).then((res)=>{
-                expect(res.body).to.be.equal(scout_);
-                done();
-            }).catch((err)=>done(err));
+        it("should create a new scout", async() => {
+         let response =  await request.post("/scouts/create-scout").send(scout_);
+         expect(response.status).to.equal(201);
+         expect(response.body.ok).to.equal(true);
         });
-        it("shouldn't create a duplicate scout", (done) => {
-            request(app)
-            .post("/api/scouts/create-scout").send(scout_).expect(400).then((res)=>{
-                expect(res.body.msg).to.be.equal("El Scout ya existe con ese email.");
-                done();
-            }).catch((err)=>done(err));
-        });
-
+        it("shouldn't create a duplicate scout", async() => {
+        let response =   await request.post("/api/scouts/create-scout").send(scout_);
+        expect(response.status).to.equal(400);
+        expect(response.body).to.equal({ok:false,msg:"Not found"});
     });
+});
+    describe("ReadScout", () => {
+        it("should create a new scout", async() => {
+         let response =  await request.post("/scouts/create-scout").send(scout_);
+         expect(response.status).to.equal(201);
+         expect(response.body.ok).to.equal(true);
+        });
+        it("shouldn't create a duplicate scout", async() => {
+        let response =   await request.post("/api/scouts/create-scout").send(scout_);
+        expect(response.status).to.equal(400);
+        expect(response.body).to.equal({ok:false,msg:"Not found"});
+    });
+});
+
 
 });
--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 const {validationResult} = require("express-validator");
-const validarCampos = (req,next,res=response)=>{
+const validarCampos = (req,res=response,next)=>{
     const errors=validationResult(req);
     if(!errors.isEmpty()){res.status(400).json({ok:false,errors:errors.mapped()        })}
     next();
