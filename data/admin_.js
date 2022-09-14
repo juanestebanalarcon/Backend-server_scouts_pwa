4c9ef87bec05cd8a4d418820a489a5d1d2d30e9e
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-13 20:04:20 -0500

    data/admin_ creada

---
+++ data/admin_.js
@@ -0,0 +1,24 @@
+export const ADMIN_TEST0={
+    nombre: "admin1",
+    email: "admin1@example.com",
+    password:"admin1password",
+    remasAsignadas:["Rama01", "Rama02", "Rama03", "Rama04", "Rama05", "Rama06", "Rama07"]
+}
+export const ADMIN_TEST1={
+    nombre: "admin2",
+    email: "admin2@example.com",
+    password:"admin2password",
+    remasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
+}
+export const ADMIN_TEST2={
+    nombre: "admin2",
+    email: "admin2@example.com",
+    password:"admin2password",
+    remasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
+}
+export const ADMIN_TEST3={
+    nombre: "admin3",
+    email: "admin3@example.com",
+    password:"admin3password",
+    remasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
+}
