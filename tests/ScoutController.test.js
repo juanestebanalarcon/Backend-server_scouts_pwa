8515435ae373c19e08c2901034ffd4086ff5ff83
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-14 15:28:37 -0500

    test read

--- tests/ScoutController.test.js
+++ tests/ScoutController.test.js
@@ -17,7 +17,7 @@ describe("ScoutController unit tests", () => {
 });
 describe("ReadScout", () => {
     it("should return a scout", async() => {
-        let response = await request.post("/scouts/63210a4e3c2be79f4a06fe6a");
+        let response = await request.get("/scouts/63210a4e3c2be79f4a06fe6a");
         expect(response.status).to.equal(200);
         expect(response.body.ok).to.equal(true);
         });
