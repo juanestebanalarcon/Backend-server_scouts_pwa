f88a6a231029fb991db0ae8746c5198f110ed1c1
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-14 14:11:05 -0500

    login - prueba movido

--- tests/ScoutController.test.js
+++ tests/ScoutController.test.js
@@ -2,27 +2,6 @@ const {request,expect,assert} = require("../config");
 const {SCOUT_TEST0,SCOUT_TEST1,SCOUT_TEST2,SCOUT_TEST3} = require("../data/scout_");
 
 describe("ScoutController unit tests", () => {
-
-    describe("LoginScout", () => {
-        it("should log in the scout", async() => {
-         let response =  await request.post("/scouts/log-in-scout").send({email:SCOUT_TEST3.email,password:SCOUT_TEST3.password});
-         expect(response.status).to.equal(201);
-         expect(response.body.ok).to.equal(true);
-         expect(response.body.email).to.equal(SCOUT_TEST3.email);
-        });
-        it("shouldn't log in the scout - incorrect password",async() => {
-        let response =   await request.post("/scouts/log-in-scout").send({email:SCOUT_TEST3.email, password:SCOUT_TEST1.password});
-        expect(response.status).to.equal(400);
-        expect(response.body).to.equal({ok:false,msg:"La password no es válida."});
-        assert.equal(response.body.msg, "La password no es válida.");
-    });
-    it("shouldn't log in the scout - incorrect email",async() => {
-        let response =   await request.post("/scouts/log-in-scout").send({email:SCOUT_TEST1.email, password:SCOUT_TEST1.password});
-        expect(response.status).to.equal(404);
-        expect(response.body).to.equal({ok:false,msg:"El correo no existe."});
-        assert.equal(response.body.msg, "El correo no existe.");
-    });
-});
     describe("CreateScout", () => {
         it("should create a new scout", async() => {
          let response =  await request.post("/scouts/create-scout").send(SCOUT_TEST0);
@@ -80,5 +59,25 @@ describe("ReadScout", () => {
             expect(response.status).to.equal(404);
             expect(response.body).to.equal({ok:false,msg:"No existe scout por ese uid."});
     });
+});
+describe("LoginScout", () => {
+    it("should log in the scout", async() => {
+     let response =  await request.post("/scouts/log-in-scout").send({email:SCOUT_TEST3.email,password:SCOUT_TEST3.password});
+     expect(response.status).to.equal(201);
+     expect(response.body.ok).to.equal(true);
+     expect(response.body.email).to.equal(SCOUT_TEST3.email);
+    });
+    it("shouldn't log in the scout - incorrect password",async() => {
+    let response =   await request.post("/scouts/log-in-scout").send({email:SCOUT_TEST3.email, password:SCOUT_TEST1.password});
+    expect(response.status).to.equal(400);
+    expect(response.body).to.equal({ok:false,msg:"La password no es válida."});
+    assert.equal(response.body.msg, "La password no es válida.");
+});
+it("shouldn't log in the scout - incorrect email",async() => {
+    let response =   await request.post("/scouts/log-in-scout").send({email:SCOUT_TEST1.email, password:SCOUT_TEST1.password});
+    expect(response.status).to.equal(404);
+    expect(response.body).to.equal({ok:false,msg:"El correo no existe."});
+    assert.equal(response.body.msg, "El correo no existe.");
+});
 });
 });
