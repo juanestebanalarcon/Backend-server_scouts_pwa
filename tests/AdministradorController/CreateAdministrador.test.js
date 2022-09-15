742cb6563863958afe0e0e46bd222f1368efe35a
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-15 14:54:28 -0500

    CreateAdminTests

--- tests/AdministradorController/CreateAdministrador.test.js
+++ tests/AdministradorController/CreateAdministrador.test.js
@@ -8,7 +8,7 @@ describe("AdminController unit tests", () => {
          expect(response.body.ok).to.equal(true);
         });
         it("shouldn't create a duplicate admin", async() => {
-        let response =   await request.post("/admin/create-ADMIN").send(ADMIN_TEST0);
+        let response =   await request.post("/admin/create-admin").send(ADMIN_TEST0);
         expect(response.status).to.equal(400);
         expect(response.body).to.equal({ok:false,msg:"El Admin ya existe con ese email."});
         assert.equal(response.body.msg, "El Admin ya existe con ese email.");
