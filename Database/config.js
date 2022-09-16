7b68e7a832ea6c849dce617838defe77aecb273b
Juan-Camilo-Tabarquino <juancatabarquino@gmail.com>
2022-09-16 16:33:05 -0500

    Solucionado error revalidar token

--- Database/config.js
+++ Database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try {
-        await mongoose.connect(process.env.DB_CNNECTION,{useNewUrlParser: true,useUnifiedTopology: true});
+        mongoose.connect(process.env.DB_CNNECTION,{useNewUrlParser: true,useUnifiedTopology: true});
         console.log('DB Online')
     } catch (error) {
        console.log(error); 
