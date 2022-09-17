b7db69f2b35b856ddd1dc1b923fd870a8baed1f8
Juan Camilo Marin Escandon <juan.marnn@uao.edu.co>
2022-09-16 23:24:37 -0500

    Datos Admin

--- data/admin_.js
+++ data/admin_.js
@@ -3,28 +3,28 @@ let ADMIN_TEST0={
     apellido: "admin0apellido",
     email: "admin0@example.com",
     password:"admin0password",
-    remasAsignadas:["Rama01", "Rama02", "Rama03", "Rama04", "Rama05", "Rama06", "Rama07"]
+    ramasAsignadas:["Rama01", "Rama02", "Rama03", "Rama04", "Rama05", "Rama06", "Rama07"]
 }
 let ADMIN_TEST1={
     nombre: "admin1",
     apellido: "admin1apellido",
     email: "admin1@example.com",
     password:"admin1password",
-    remasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
+    ramasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
 }
 let ADMIN_TEST2={
     nombre: "admin2",
     apellido: "admin2apellido",
     email: "admin2@gmail.com",
     password:"admin2password",
-    remasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
+    ramasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
 }
 let ADMIN_TEST3={
     nombre: "admin3",
     apellido: "admin3apellido",
     email: "admin3@example.com",
     password:"admin3password",
-    remasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
+    ramasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
 }
 let ADMIN_TEST4={
     nombre: "admin4",
@@ -38,6 +38,6 @@ let ADMIN_TEST5={
     apellido: "admin5apellido",
     email: "admin5@example.com",
     password:"admin5password",
-    remasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
+    ramasAsignadas:["Rama001", "Rama002", "Rama003", "Rama004", "Rama005", "Rama006", "Rama007"]
 }
 module.exports= {ADMIN_TEST0,ADMIN_TEST1,ADMIN_TEST2,ADMIN_TEST3,ADMIN_TEST4,ADMIN_TEST5}
