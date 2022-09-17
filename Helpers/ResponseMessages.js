31283ddce7efc40c74ada44bf61edfa423876529
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-17 11:41:40 -0500

    Helpers/ResponseMessages.js agregado

---
+++ Helpers/ResponseMessages.js
@@ -0,0 +1,10 @@
+let RESPONSE_MESSAGES={
+        ERR_ALREADY_EXISTS:`Registro ya existe.`,
+        ERR_500:"Error interno del servidor",
+        ERR_NOT_FOUND:"No existe(n) registro(S) solicitado(S)",
+        ERR_EMAIL_NOT_FOUND:"No existe correo",
+        ERR_INVALID_PASSWORD:"Contraseña incorrecta",
+        SUCCESS_2XX:"Transacción ejecutada exitosamente"
+    }
+
+module.exports ={RESPONSE_MESSAGES}
