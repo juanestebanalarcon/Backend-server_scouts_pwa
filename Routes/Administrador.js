65ba621b8f26ef5de1b5a1dcd59d5ae11d88c67a
Diana Marcela Garcia Lopez <diana_ma.garcia@uao.edu.co>
2022-09-17 00:52:29 -0500

    Update Administrador.js

--- Routes/Administrador.js
+++ Routes/Administrador.js
@@ -8,8 +8,8 @@ const router = Router();
 router.post("/create-admin",[
     check("nombre","Nombre es obligatorio").not().isEmpty(),
     check("email","Email es obligatorio").isEmail(),
-    check("password","Password es obligatorio").not().isEmpty(),
-    //check("ramaAsociada","rama asociada es obligatorio").not().isEmpty(),
+    // check("password","Password es obligatorio").not().isEmpty(),
+    check("ramaAsociada","rama asociada es obligatorio").not().isEmpty(),
     validarCampos,
 ],createAdmin);
 router.post('/log-in-admin',[
