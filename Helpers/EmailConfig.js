adad9bbc61581005a8bd43c31d9e1ec816082460
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-16 22:35:26 -0500

    accessToken

--- Helpers/EmailConfig.js
+++ Helpers/EmailConfig.js
@@ -11,7 +11,8 @@ const transporter = nodemailer.createTransport({
         pass: process.env.MAIL_PASSWORD,
         clientId: process.env.OAUTH_CLIENTID,
         clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        //refreshToken: process.env.OAUTH_REFRESH_TOKEN
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        accessToken: process.env.OAUTH_ACCESS_TOKEN
     }
 });
 
--- tests/ScoutController/LoginScout.test.js
+++ tests/ScoutController/LoginScout.test.js
@@ -9,16 +9,16 @@ describe("LoginScout", () => {
      expect(response.body.ok).to.equal(true);
      expect(response.body.email).to.equal(SCOUT_TEST3.email);
     });
+    it("shouldn't log in the scout - incorrect email",async() => {
+        let response =   await request.post("/scouts/log-in-scout").send({email:"scout10@correo.com", password:"12345678Test01"});
+        expect(response.status).to.equal(404);
+       // expect(response.body.msg).to.equal('El correo no existe.');
+    });
     it("shouldn't log in the scout - incorrect password",async() => {
     let response =   await request.post("/scouts/log-in-scout").send({email:SCOUT_TEST3.email, password:SCOUT_TEST1.password});
     expect(response.status).to.equal(400);
     expect(response.body.msg).to.equal('La password no es válida.');
 
-});
-it("shouldn't log in the scout - incorrect email",async() => {
-    let response =   await request.post("/scouts/log-in-scout").send({email:"scout10@correo.com", password:"12345678Test01"});
-    expect(response.status).to.equal(404);
-   // expect(response.body.msg).to.equal('El correo no existe.');
 });
 });
 });
