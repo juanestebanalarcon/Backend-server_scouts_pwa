dc658099f7cc736cdda8a458df83f3b9f92a465b
juanestebanalarcon <jalarcon_logyca@outlook.com>
2022-09-16 19:26:42 -0500

    añadido

---
+++ Helpers/randomPassowrd.js
@@ -0,0 +1,11 @@
+const  generateRandomPass = (num) => {
+    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result1= ' ';
+    const charactersLength = characters.length;
+    for ( let i = 0; i < num; i++ ) {
+        result1 += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result1;
+}
+module.exports = {generateRandomPass}
